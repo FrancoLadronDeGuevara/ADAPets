6d3155a5c97c478619022f62386ea2443b0fc3d0
FrancoLadronDeGuevara <francoguevara93@gmail.com>
2026-02-02 17:56:29 -0300

    Added variables on css and styles for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,39 @@
+import Logo from "../../assets/Logo/logo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="bg-vet-dark text-vet-text p-4 fixed w-full">
+      <nav className="flex justify-between items-center">
+        <a href="/">
+          <img src={Logo} alt="Logo ADAPets" width="64px" />
+        </a>
+
+        <ul className="flex items-center gap-3">
+          <li>
+            <a className="text-sm font-medium" href="#">
+              SERVICIOS
+            </a>
+          </li>
+          <hr className="w-0.5 h-4 bg-vet-bg" />
+          <li>
+            <a className="text-sm font-medium shadow" href="#">
+              NOSOTRAS
+            </a>
+          </li>
+          <hr className="w-0.5 h-4 bg-vet-bg" />
+          <li className="hover:text-vet-accent">
+            <a className="text-sm font-medium" href="#">
+              CONTACTO
+            </a>
+          </li>
+        </ul>
+
+        <button className="bg-vet-accent px-4 py-2 rounded hover:bg-vet-primary-dark ">
+          Pedir turno
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
